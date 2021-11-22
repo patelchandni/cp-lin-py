737adc9173b8a5c8cfe6447835ae33324ec574e3
Chandni Patel <88407036+patelchandni@users.noreply.github.com>
2021-11-22 16:51:05 -0600

    Update __init__.py

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "vs default 1111111111 ==> This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "ga con rfp=0 ==> This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
